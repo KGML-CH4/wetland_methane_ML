4ad2c97495e1c3b75f94253d9e7af6ce5047ba8f
Chris Smith <chriscs@colorado.edu>
2026-03-28 17:55:38 -0400

    added domain adapt nn (#18)
    Co-authored-by: chriscrsmith <chriscs@uoregon.edu>

--- Code/nn_architectures.py
+++ Code/nn_architectures.py
@@ -62,13 +62,10 @@ class model_stack_wCNN(nn.Module):
         return weight.new_zeros(self.nlayers, bsz, self.nhid)
         
     
-class baseline(nn.Module):
+class gru(nn.Module):
     def __init__(self, ninp, nhid=8, nlayers=2, nout1=1, dropout=0):
         super(pureML_GRU, self).__init__()
-        if nlayers > 1:
-            self.gru = nn.GRU(ninp, nhid,nlayers,dropout=dropout, batch_first=True)
-        else:
-            self.gru = nn.GRU(ninp, nhid,nlayers, batch_first=True)
+        self.gru = nn.GRU(ninp, nhid,nlayers,dropout=dropout, batch_first=True)
         self.densor_flux = nn.Linear(nhid, nout1)
         self.nhid = nhid
         self.nlayers = nlayers
@@ -93,3 +90,65 @@ class baseline(nn.Module):
 
 
     
+class grad_reverse(torch.autograd.Function):
+    # https://discuss.pytorch.org/t/solved-reverse-gradients-in-backward-pass/3589/2
+    # https://medium.com/@lukas.hauzenberger/adverserial-training-5bb5ea919ae7
+    @staticmethod
+    def forward(ctx, input_, lmbda=100):
+        ctx.lmbda = lmbda
+        return input_.view_as(input_)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        grad_input = grad_output.neg() * ctx.lmbda
+        #grad_input = grad_output * ctx.lmbda
+        return grad_input, None
+
+class feature_extractor(nn.Module):
+    def __init__(self, size, nhid):
+        super(feature_extractor, self).__init__()
+        self.gru = nn.GRU(input_size=size, hidden_size=nhid, num_layers=2, batch_first=True)
+
+    def forward(self, inputs):
+        output, _ = self.gru(inputs)
+        return output    
+
+class output_branch(nn.Module):
+    def __init__(self):
+        super(output_branch, self).__init__()
+        self.dense = nn.Linear(8,1)
+
+    def forward(self, inputs):
+        output = self.dense(inputs)                
+        return output
+
+class domain_classifier(nn.Module):
+    def __init__(self, lambda_):
+        super(domain_classifier, self).__init__()
+        self.fc1 = nn.Linear(8, 4) 
+        self.fc2 = nn.Linear(4, 1)  # 1 output per month
+        self.fc_out = nn.Linear(24, 1)  # 12-mo window
+        self.ReLU=nn.ReLU()
+        self.lambda_ = lambda_
+        
+    def forward(self, x):
+        x = grad_reverse.apply(x, self.lambda_)
+        x = self.ReLU(self.fc1(x))
+        x = self.ReLU(self.fc2(x))  # 1 output per month
+        x = x.view(x.shape[0], -1)  # flatten        
+        x = self.fc_out(x)  # 1 over output for the training example
+        x = torch.sigmoid(x)
+        return x
+
+class doman_adapt(nn.Module):
+    def __init__(self, input_dim, hidden_dim, lambda_):
+        super(combined_nn, self).__init__()
+        self.feature_extractor = feature_extractor(input_dim, hidden_dim)
+        self.output_branch = output_branch()
+        self.domain_classifier = domain_classifier(lambda_)
+
+    def forward(self, x):
+        features = self.feature_extractor(x)
+        output = self.output_branch(features)
+        domain = self.domain_classifier(features)
+        return output, domain  # torch.Size([bsz, 12, 1]) torch.Size([bsz, 12, 1])    
